0cdf2e714c5eb08d03b883f35760758d1e359b24
Pierre Roy <lrp.roy@gmail.com>
2024-04-13 18:07:26 +0200

    Grab rotation and position interpolation

--- modules/interaction/scripts/interactor_item_holder.gd
+++ modules/interaction/scripts/interactor_item_holder.gd
@@ -1,17 +1,21 @@
 extends Node3D
 class_name InteractorItemHolder
 
-@export var drop_interaction: Interaction
 @export var can_drop: bool = true
+@export var drop_interaction: Interaction
 @export var drop_delay: float = 0.1
+@export var rotate_speed: float = 2.0
+@export var move_speed: float = 2.0
 
 var item: Node3D
 
-func _process(_delta: float) -> void:
+func _process(delta: float) -> void:
     if !item:
        return
-    item.position = global_position
-    item.rotation = global_rotation
+    item.position = item.position.slerp(global_position, delta * 10 * move_speed)
+    item.rotation.y = lerp_angle(item.rotation.y, global_rotation.y, delta * 10 * rotate_speed)
+    item.rotation.x = lerp_angle(item.rotation.x, global_rotation.x, delta * 10 * rotate_speed)
+    item.rotation.z = lerp_angle(item.rotation.z, global_rotation.z, delta * 10 * rotate_speed)
 
 func drop(interactor: Interactor) -> void:
     interactor.pop(drop_interaction)
