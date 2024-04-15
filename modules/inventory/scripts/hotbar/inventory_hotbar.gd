e12caaf14f8c9ce7fbd68c82110812436033122f
Pierre Roy <lrp.roy@gmail.com>
2024-04-15 22:33:32 +0200

    Looted items binded to first hotbar empty slot

--- modules/inventory/scripts/hotbar/inventory_hotbar.gd
+++ modules/inventory/scripts/hotbar/inventory_hotbar.gd
@@ -1,16 +1,39 @@
 extends Node
 class_name InventoryHotbar
 
-signal item_added(slot: InventoryHotbarSlot)
-signal item_removed(slot: InventoryHotbarSlot)
+signal item_binded(slot: InventoryHotbarSlot)
+signal item_unbinded(slot: InventoryHotbarSlot)
 
 @export var slot_count: int = 4
 
+var inventory: Inventory
 var slots: Array[InventoryHotbarSlot] = []
 
-func _init() -> void:
+func _ready() -> void:
+    inventory = get_parent()
+    if inventory == null:
+        queue_free()
+    else:
+        _create_slots()
+        inventory.item_added.connect(_on_item_added)
+        inventory.item_removed.connect(_on_item_removed)
+
+func _create_slots() -> void:
     for i in slot_count:
         slots.append(InventoryHotbarSlot.new())
+
+func _on_item_added(item: InventoryItem) -> void:
+    for slot in slots:
+        if slot.is_empty():
+            slot.item = item
+            item_binded.emit(slot)
+            return
+
+func _on_item_removed(item: InventoryItem) -> void:
+    for slot in slots:
+        if slot.item == item:
+            slot.item = null
+            item_unbinded.emit(slot)
 
 func is_empty() -> bool:
     for slot in slots:
--- modules/inventory/scripts/hotbar/inventory_hotbar_view.gd
+++ modules/inventory/scripts/hotbar/inventory_hotbar_view.gd
@@ -5,27 +5,35 @@ class_name InventoryHotbarView
 @export var slot_container: Container
 @export var hide_if_empty: bool = true
 
-var inventory_hotbar: InventoryHotbar
+var hotbar: InventoryHotbar
 var slot_views: Array[InventoryHotbarSlotView]
 
 func _ready() -> void:
     _clear()
     var inventory := PlayerModule.player.inventory
-    inventory_hotbar = inventory.get_node_or_null("%InventoryHotbar")
-    if !inventory_hotbar:
+    hotbar = inventory.get_node_or_null("%InventoryHotbar")
+    if !hotbar:
         queue_free()
     else:
         _create_slots()
         _update()
+        hotbar.item_binded.connect(_on_item_binded)
+        hotbar.item_unbinded.connect(_on_item_unbinded)
+
+func _on_item_binded(_slot: InventoryHotbarSlot) -> void:
+    _update()
+
+func _on_item_unbinded(_slot: InventoryHotbarSlot) -> void:
+    _update()
 
 func _update() -> void:
-    if hide_if_empty && inventory_hotbar.is_empty():
+    if hide_if_empty && hotbar.is_empty():
         hide()
     else:
         show()
 
 func _create_slots() -> void:
-    for slot in inventory_hotbar.slots:
+    for slot in hotbar.slots:
         var view := slot_view.instantiate() as InventoryHotbarSlotView
         view.bind(slot)
         slot_views.append(view)
