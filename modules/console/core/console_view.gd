4531c81d367b4b70dbd6048aed13cd0995ca500a
Pierre Roy <lrp.roy@gmail.com>
2024-04-22 22:27:35 +0200

    Console base system imported from Project Primer

---
+++ modules/console/core/console_view.gd
@@ -0,0 +1,29 @@
+extends Control
+class_name ConsoleView
+
+@onready var console: Console = get_parent()
+@onready var console_text: RichTextLabel = %RichTextLabel
+@onready var input_field: LineEdit = %LineEdit
+
+func _ready() -> void:
+    console.opened.connect(_on_console_opened)
+    console.closed.connect(_on_console_closed)
+    z_index = 1000
+    hide()
+
+func _process(_delta: float) -> void:
+    if Input.is_action_just_pressed("ui_text_submit"):
+        _submit()
+
+func _submit() -> void:
+    console.submit(input_field.text)
+    input_field.clear()
+
+func _on_console_opened() -> void:
+    input_field.clear()
+    input_field.call_deferred("grab_focus")
+    show()
+
+func _on_console_closed() -> void:
+    input_field.release_focus()
+    hide()
