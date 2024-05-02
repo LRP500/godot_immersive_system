8fb6fdc97a6eb0085cf94ca5dbad866777395f4c
Pierre Roy <lrp.roy@gmail.com>
2024-05-03 01:28:29 +0200

    Console help command reworked

--- modules/console/core/console.gd
+++ modules/console/core/console.gd
@@ -11,11 +11,10 @@ var commands: Dictionary = {}
 var is_open: bool = false
 
 func _init() -> void:
-	add_command("list", list)
-	add_command("help", help)
-	add_command("quit", quit)
-	add_command("clear", clear)
-	add_command("close", close)
+	add_command("help", "Provides help information for console commands.", help)
+	add_command("quit", "Quits the application.", quit)
+	add_command("clear", "Clears the console's view.", clear)
+	add_command("close", "Closes the console.", close)
 
 func _ready() -> void:
 	close()
@@ -24,8 +23,8 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(toggle_action_name):
 		toggle()
 
-func add_command(in_name: String, method: Callable, param_count: int = 0) -> void:
-	commands[in_name] = ConsoleCommand.new(method, param_count)
+func add_command(_name: String, desc: String, method: Callable, param_count: int = 0) -> void:
+	commands[_name] = ConsoleCommand.new(method, param_count, desc)
 
 # TODO: Display error message if command doesn't exist.
 func execute(command: ConsoleCommand, args: PackedStringArray) -> bool:
@@ -61,21 +60,20 @@ func toggle() -> void:
 	else:
 		open()
 
+func print_line(text: String) -> void:
+	view.console_text.add_text(text + "\n")
+
 #region Commands
 
-# TODO: Display one command per line with short description.
-func list() -> void:
-	var items := []
-	for command: String in commands:
-		items.append(str(command))
-	items.sort()
-	view.console_text.add_text(str(items) + "\n")
+func help() -> void:
+	print_line("For more information on a specific command, type HELP command-name")
+	for key: String in commands:
+		var command: ConsoleCommand = commands[key]
+		var text: String = "%s\t\t\t%s" % [key.to_upper(), command.description]
+		print_line(text)
 
 func clear() -> void:
 	view.console_text.clear()
-
-func help() -> void:
-	pass
 
 func quit() -> void:
 	get_tree().quit()
--- modules/console/core/console_command.gd
+++ modules/console/core/console_command.gd
@@ -3,7 +3,9 @@ class_name ConsoleCommand
 
 var method: Callable
 var param_count: int
+var description: String
 
-func _init(_method: Callable, _param_count: int) -> void:
+func _init(_method: Callable, _param_count: int, _description: String) -> void:
     method = _method
     param_count = _param_count
+    description = _description
