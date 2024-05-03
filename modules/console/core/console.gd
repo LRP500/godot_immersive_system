42201ea4c3131b87c5920e27a4592cff51ca1619
Pierre Roy <lrp.roy@gmail.com>
2024-05-03 13:49:28 +0200

    Console sanitization fix + restart command (disabled)

--- modules/console/core/console.gd
+++ modules/console/core/console.gd
@@ -15,6 +15,7 @@ func _init() -> void:
 	add_command("quit", "Quits the application.", quit)
 	add_command("clear", "Clears the console's view.", clear)
 	add_command("close", "Closes the console.", close)
+	# add_command("restart", "Restarts the current scene.", restart)
 
 func _ready() -> void:
 	close()
@@ -26,10 +27,7 @@ func _input(event: InputEvent) -> void:
 func add_command(_name: String, desc: String, method: Callable, param_count: int = 0) -> void:
 	commands[_name] = ConsoleCommand.new(method, param_count, desc)
 
-# TODO: Display error message if command doesn't exist.
 func execute(command: ConsoleCommand, args: PackedStringArray) -> bool:
-	if command == null:
-		return false
 	if command.param_count > 0:
 		command.method.call(args)
 	else:
@@ -37,14 +35,22 @@ func execute(command: ConsoleCommand, args: PackedStringArray) -> bool:
 	return true
 
 func submit(input: String) -> void:
+	if input.is_empty():
+		return
 	input = sanitize(input)
 	var command_name := input.split(' ')[0]
 	var args := input.split(' ').slice(1)
 	var command: ConsoleCommand = commands.get(command_name)
+	if command == null:
+		print_error("'%s' is not recognized as an internal or external command." % command_name);
+		print_line("Type 'help' for a list of available commands.")
+		return
 	execute(command, args)
 
 func sanitize(input: String) -> String:
-	return input.replace("\n", "")
+	input = input.strip_escapes()
+	input = input.strip_edges()
+	return input
 
 func open() -> void:
 	is_open = true
@@ -61,12 +67,17 @@ func toggle() -> void:
 		open()
 
 func print_line(text: String) -> void:
+	view.console_text.push_color(Color.WHITE)
+	view.console_text.add_text(text + "\n")
+
+func print_error(text: String) -> void:
+	view.console_text.push_color(Color.RED)
 	view.console_text.add_text(text + "\n")
 
 #region Commands
 
 func help() -> void:
-	print_line("For more information on a specific command, type HELP command-name")
+	print_line("For more information on a specific command, type 'help [command-name]'.")
 	for key: String in commands:
 		var command: ConsoleCommand = commands[key]
 		var text: String = "%s\t\t\t%s" % [key.to_upper(), command.description]
@@ -78,4 +89,10 @@ func clear() -> void:
 func quit() -> void:
 	get_tree().quit()
 
+func restart() -> void:
+	get_tree().reload_current_scene()
+
 #endregion Commands
+
+# TODO: Add line prompt
+# TODO: Add restart command
