cd495e1f8d6d3b5d3e735635e31ce59ddf490f89
Pierre Roy <lrp.roy@gmail.com>
2024-05-06 13:02:48 +0200

    Moved player to its own collision layer

--- modules/weapon/core/projectile/projectile.gd
+++ modules/weapon/core/projectile/projectile.gd
@@ -13,3 +13,8 @@ func _process(_delta: float) -> void:
 func _on_hitbox_body_entered(body: Node3D) -> void:
 	print("body: %s" % body)
 	queue_free()
+
+func _on_hitbox_area_entered(_area:Area3D) -> void:
+	pass
+
+# TODO: Collision with raycast instead of hitbox
