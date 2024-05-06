07eb31c4e36b9a6034854b36ecb703f5eb8a4118
Pierre Roy <lrp.roy@gmail.com>
2024-05-06 19:54:06 +0200

    Pulse firing mode

--- modules/weapon/core/firing_mode/pulse_firing_mode.gd
+++ modules/weapon/core/firing_mode/pulse_firing_mode.gd
@@ -1,2 +1,22 @@
 extends WeaponFiringMode
 class_name PulseFiringMode
+
+@export var charge_time: float = 1.0
+
+var pulse_timer: Timer
+
+func _on_init() -> void:
+    pulse_timer = Timer.new()
+    pulse_timer.one_shot = true
+    pulse_timer.wait_time = charge_time
+    weapon.add_child(pulse_timer)
+    pulse_timer.timeout.connect(_on_pulse_timer_timeout)
+
+func _on_fire_just_pressed() -> void:
+    pulse_timer.start()
+
+func _on_fire_just_released() -> void:
+    pulse_timer.stop()
+
+func _on_pulse_timer_timeout() -> void:
+    fire.emit()
